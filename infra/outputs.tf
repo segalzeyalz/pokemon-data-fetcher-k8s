e2580a22e879d4c7d3f1aa1863ac3497eda9ed2c
eyal-dcoya <eyal@dcoya.com>
2024-07-14 10:46:18 +0300

    without the terrform

--- infra/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubeconfig" {
-  value = google_container_cluster.primary.kubeconfig_raw
-  sensitive = true
-}
